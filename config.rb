ec93da0cb48c2c354e558ad17c0ea86f1f6618b6
Aeris <aeris@imirhil.fr>
2014-03-31 23:31:39 +0200

    Initial commit

---
+++ config.rb
@@ -0,0 +1,10 @@
+activate :livereload
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
