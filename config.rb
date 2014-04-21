ff922e7c99774bbf542bf39ce44b7ccae8c20caa
Aeris <aeris@imirhil.fr>
2014-04-21 16:00:38 +0200

    Atom feed

--- config.rb
+++ config.rb
@@ -3,6 +3,7 @@ activate :livereload
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
+set :site_url, 'https://xn--caf-vie-prive-dhbj.fr'
 
 configure :build do
 	activate :minify_css
